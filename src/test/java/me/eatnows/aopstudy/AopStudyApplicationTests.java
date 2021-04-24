e2e15de46ab332dd36b4889d1be326aaa99184a6
eatnows <gagyasindev@gmail.com>
2021-04-24 17:35:49 +0900

    init project

---
+++ src/test/java/me/eatnows/aopstudy/AopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eatnows.aopstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
